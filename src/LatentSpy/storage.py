1569a6e715ced48912c8ea5e8a4aba0170951744
Chyavan Shenoy <chyavandshenoy2318@gmail.com>
2026-03-10 10:38:09 +0530

    Updated result storing naming convention

--- src/LatentSpy/storage.py
+++ src/LatentSpy/storage.py
@@ -3,13 +3,10 @@ from collections import defaultdict
 class MetricStorage:
     def __init__(self):
         self.history = defaultdict(lambda: defaultdict(list))
-        self.step = 0
-    
-    def update(self,results):
+    def update(self, results, step: int):
         for layer_name, metrics in results.items():
             for metric_name, value in metrics.items():
-                self.history[layer_name][metric_name].append(value)
-        self.step += 1
+                self.history[layer_name][metric_name].append((step, value))
 
     def get_history(self):
         return self.history
